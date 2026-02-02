6a6fcd7ebacd99ba4e28ff55b21143c6c9947a58
KhalidDev <148249284+devAlq@users.noreply.github.com>
2026-02-02 15:15:27 +0300

    Update IO-management-project.c

--- IO-management-project.c
+++ IO-management-project.c
@@ -1,31 +1,87 @@
 #include <stdio.h>
-#include <stdlib.h>
+void read() {
+    FILE* fptr;
+    fptr = fopen("file.txt", "r");
+    if (fptr == NULL) {
+        printf("There was an issue opening the file \n");
+        return;
+    }
+
+    char line[256];
+
+    while (fgets(line, sizeof(line), fptr)) {
+        printf("%s", line);
+    }
+
+    fclose(fptr);
+
+
+
+}
+
+void writeToFile(const char info[20]) {
+    FILE* fptr;
+    fptr = fopen("file.txt", "a");
+
+    if (fptr == NULL) {
+        printf("File pointer is NULL\n");
+        return;
+    }
+
+    fprintf(fptr, "%s\n", info);
+    fclose(fptr);
+
+}
+void deleteFile(){
+
+    if (remove("file.txt") != 0) {
+        printf("There was an issue deleting the file.\n");
+    }
+
+    printf("File deleted successfully.\n");
+}
+
+
 
 int main() {
-    FILE *source, *destination;
-    char ch;
+  
+    FILE* fptr;
 
-    source = fopen("file.txt", "r");
-    if (source == NULL) {
-        perror("Error opening source file");
+    fptr = fopen("file.txt", "w");
+    if (fptr == NULL) {
+        printf("There was an issue creating the file \n");
         return 1;
     }
+    printf("File created successfully! \n");
+    fclose(fptr);
 
-    destination = fopen("copy.txt", "w");
-    if (destination == NULL) {
-        perror("Error opening destination file");
-        fclose(source);
-        return 1;
+    
+    int number = 0;
+    char info[20];
+    while (number != 4){
+        printf("1: read\n");
+        printf("2: write\n");
+        printf("3: delete\n");
+        printf("4: quit\n");
+
+        scanf("%d", &number);
+
+        if(number == 1){
+            read();
+        }else if(number == 2){
+              printf("Enter what do you want to write: ");
+              scanf("%s", info);
+              writeToFile(info);
+        }else if(number == 3){
+            deleteFile();
+            number = 4;
+            
+        }
     }
 
-    while ((ch = fgetc(source)) != EOF) {
-        fputc(ch, destination);
-    }
 
-    // Close files
-    fclose(source);
-    fclose(destination);
 
-    printf("File copied successfully.\n");
-    return 0;
+
+
+    
 }
